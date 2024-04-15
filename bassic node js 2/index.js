ce63d6bed93aa2f2e5d61a84343903e1525c0043
AkbarRahmat <146505525+AkbarRahmat@users.noreply.github.com>
2024-04-16 00:42:37 +0700

    Add files via upload

---
+++ bassic node js 2/index.js
@@ -0,0 +1,13 @@
+// looping for , while, do while
+for (let i = 0;i<5;i++){
+    console.log(i)
+}
+nama = 'Akbar'
+let i = 0
+
+while (kondisi){
+    if (nama [i] == 'r') kondisi = false;
+    console.log(nama[i],kondisi);
+    i++;
+}
+
